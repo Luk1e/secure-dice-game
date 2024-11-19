7d05d7cfc98216627d7c9dc67f8bb0e4f78c6a41
Luk1e <luka.gogiashvili.02@gmail.com>
2024-11-19 11:48:30 +0400

    update: changed generateSecureInt algorithm

--- RandomGenerator.js
+++ RandomGenerator.js
@@ -9,24 +9,7 @@ class RandomGenerator {
     if (min >= max) {
       throw new Error("max must be greater than min");
     }
-
-    const range = max - min + 1;
-    const bitsNeeded = Math.ceil(Math.log2(range));
-    const bytesNeeded = Math.ceil(bitsNeeded / 8);
-    const mask = (1 << bitsNeeded) - 1;
-
-    while (true) {
-      const bytes = crypto.randomBytes(bytesNeeded);
-      let num = 0;
-      for (let i = 0; i < bytesNeeded; i++) {
-        num = (num << 8) | bytes[i];
-      }
-
-      num = num & mask;
-      if (num < range) {
-        return num + min;
-      }
-    }
+    return crypto.randomInt(min, max + 1);
   }
 
   calculateHMAC(key, message) {
